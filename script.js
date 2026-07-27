977a560e09acfcf17b33a72e8221b2a8d93d8027
GEOD-TRANSF <taibihabib@ymail.com>
2026-07-27 17:30:42 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// ======================================================
+// GEO_TRANS
+// Développé par : Hebib Taibi - ENSGTS - Arezew
+//  hebib.taibi@ensgts.dz
+// ======================================================
+
+console.log("Bienvenue sur GEO_TRANS");
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Site GEO_TRANS chargé avec succès.");
+});
